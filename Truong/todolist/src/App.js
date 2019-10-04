e24f61d9e459f59d315bd1e0e8e424b0fd238ac1
truonghdpk <truongondaihoc@gmail.com>
2019-10-04 17:34:13 +0700

    Fixed some things

--- Truong/todolist/src/App.js
+++ Truong/todolist/src/App.js
@@ -20,11 +20,19 @@ class Showlist extends React.Component {
   constructor(props){
     super(props);
     console.log("Run Showlist");
+    this.willdel = this.willdel.bind(this);
+  }
+  willdel(event){
+    
   }
   render() {
     var showlistcache = detais.map(i =>{
       return (
-        <li class="list-group-item list-group-item-action">{i.detail}.  {i.value}</li>
+        <li className="list-group-item list-group-item-action">{i.detail}.  {i.value}
+          <button onclick={this.willdel} type="button" className="close" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </li>
       );
     });
     return showlistcache;
@@ -41,6 +49,7 @@ class Forminputlist extends React.Component {
     cache = new Object();
     cache.value = event.target.value;
     cache.detail = detais.length + 1;
+    console.log("Cachedeatil : " +cache.detail);
   }
   onSubmit(event){
     event.preventDefault();
@@ -58,7 +67,7 @@ class Forminputlist extends React.Component {
       return (
         <form ref="form" onSubmit={this.onSubmit} className="bgform">
             <div class="input-group input-group-lg setwidth">
-              <input onChange={this.setvalue} check="checktome" type="text"  className="input form-control" aria-label="" aria-describedby="inputGroup-sizing-sm"/>
+              <input onKeyDown={this.setvalue} check="checktome" type="text"  className="input form-control" aria-label="" aria-describedby="inputGroup-sizing-sm"/>
             </div>
         </form>
       )
@@ -68,6 +77,7 @@ class FORM extends Component {
   constructor(props){
     super(props);
     this.state = {detail: detais}
+    console.log(this.addItem.bind(this));
     this.addItem = this.addItem.bind(this);
     console.log("chay Form");
   }
@@ -81,11 +91,17 @@ class FORM extends Component {
     }
     this.setState({detail: detais});
   }
+  remove(event){
+    var comparedel = event;
+    console.log(event);
+    detais.splice(comparedel.detail,1);
+    this.setState({detail: detais});
+  }
   render() {
     return (
       <div className="marginbody">
         <ul class="list-group">
-          <Showlist />
+          <Showlist remove={this.remove} />
         </ul>
         <div className="spaceinput"></div>
         <Forminputlist addItem = {this.addItem} />

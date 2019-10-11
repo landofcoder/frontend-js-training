61e50a2556f2fc9de9a713c7c9a247a8e4d615b9
Chien <chien@autoketing.com>
2019-10-11 13:16:50 +0700

    - Add example

--- code-example/src/App.js
+++ code-example/src/App.js
@@ -1,35 +1,17 @@
 import React from 'react';
 import './App.css';
-import Header from "./Header";
+import Sum from './components/sum/Sum';
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      inputValue: 'JAY',
-      student: [
-        {}
-      ]
-    }
-  }
-
-  onChangeValue = (e) => {
-    this.setState({inputValue: e.target.value})
   }
 
   render() {
-    const { inputValue } = this.state;
     return (
         <>
-          <div>
-            <Header/>
-            <input value={inputValue} onChange={this.onChangeValue}/>
-
-            <h4>{inputValue}</h4>
-
-            <h1>{inputValue}</h1>
-          </div>
+          <Sum/>
         </>
     );
   }
---
+++ code-example/src/sagas/main-saga.js
@@ -0,0 +1,4 @@
+function* mainSaga() {
+
+}
+export default mainSaga;

37a49c6f4b04c894136213724370f2027bbd5c29
Chien <chien@autoketing.com>
2019-10-23 16:42:35 +0700

    - Add example saga

--- code-example/src/sagas/main-saga.js
+++ code-example/src/sagas/main-saga.js
@@ -1,4 +1,17 @@
-function* mainSaga() {
+import {takeEvery, put} from "redux-saga/effects";
+import * as types from '../constants/index';
+import {checkDiscountProcessService,} from './service';
 
+function* getApiList() {
+    const payload = {testParam: 1};
+    const result = yield call(checkDiscountProcessService, payload);
+    console.log('result:', result);
+    // push data to reducer
+    yield put({type: types.RECEIVED_DATA_LIST, result});
 }
+
+function* mainSaga() {
+    yield takeEvery(types.GET_API_LIST, getApiList);
+}
+
 export default mainSaga;
